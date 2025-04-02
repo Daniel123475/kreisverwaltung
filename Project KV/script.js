2de6583abd8f0842113702f7dc50a5e9d8b7ff60
Daniel123475 <daniel0606h@gmail.com>
2025-04-03 00:47:44 +0200

    Add files via upload

---
+++ Project KV/script.js
@@ -0,0 +1,7 @@
+// Du kannst hier Funktionen hinzufügen, z. B. zum Öffnen einer Detailseite
+document.querySelectorAll("button").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      alert("Hier könnten weitere Infos stehen.");
+    });
+  });
+  
